388204405ca6ac7dd2071dd95cbe43cca46e501d
Henry Darnell <hjdarnel@gmail.com>
2018-06-25 16:31:29 -0500

    Allow query option to turn off bot

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,7 @@
-const getPull = require('./github.js');
+const _ = require('lodash');
 const bunyan = require('bunyan');
 const Q = require('q');
+const getPull = require('./github.js');
 const logger = bunyan.createLogger({name: 'reviewable-linker'});
 
 const parser = (message) => {
@@ -10,17 +11,26 @@ const parser = (message) => {
     };
     const promises = [];
 
-    const pullRegex = /http(?:s)?:\/\/(?:www\.)?github\.com\/([a-z, A-Z, 0-9, -]*)\/([a-z, A-Z, 0-9, -]*)\/pull\/(\d*)/gm;
+    const pullRegex = /http(?:s)?:\/\/(?:www\.)?github\.com\/([a-z, A-Z, 0-9, -]*)\/([a-z, A-Z, 0-9, -]*)\/pull\/(\d*)(?:\?bot=([a-z]*))*/gm;
     let data = pullRegex.exec(message.text);
 
     while (data != null) {
         const pull = {
             team: data[1],
             repository: data[2],
-            pullNumber: data[3]
+            pullNumber: data[3],
+            shareLink: data[4]
         };
 
-        results.pulls.push(pull);
+        if (pull.shareLink) {
+            pull.shareLink = pull.shareLink.toLowerCase();
+        }
+
+        if (_.get(pull, 'shareLink') === 'n' || _.get(pull, 'shareLink') === 'no') {
+            logger.info('Skipped link', pull);
+        } else {
+            results.pulls.push(pull);
+        }
         data = pullRegex.exec(message.text);
     }
 
